d2f8507ddcf710426beae9de810186402a1808cd
mathbook3948 <yuno.jung.07@gmail.com>
2025-09-03 00:31:18 +0900

    design: navbar border-b 적용

--- views/shared/navbar.tsx
+++ views/shared/navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 
 const Navbar = ({ appTitle }: NavbarProps) => {
   return (
-    <div className="flex flex-row items-center h-12">
+    <div className="flex flex-row items-center h-12 border-b border-b-muted-foreground/20 mb-4">
       <span className="text-2xl font-bold text-center cursor-pointer">{appTitle}</span>
     </div>
   );
